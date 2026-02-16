575c779520855ee0c8653b7e98602702d44d45a6
Yossaran Mankongdee <yossaran.p6.2.17.ccs@gmail.com>
2026-02-16 09:42:32 +0700

    p02

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,1 +1,19 @@
-export class Person {}
+export class Person {
+    public firstname: string = 'Unknown';
+    public lastname: string = 'Unknown';
+    public age: number = 0;
+    static COUNTRY: string = 'Thailand';
+
+    getFullName() {
+        console.log(`${this.firstname} ${this.lastname}`);
+    }
+
+    setAge(x: number) {
+        this.age = x;
+    }
+
+    getAge() {
+        console.log(this.age);
+    }
+
+}
