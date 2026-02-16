b0be1678dfa814a5bbb7c40a26b1c82d50dc27e7
Yossaran Mankongdee <yossaran.p6.2.17.ccs@gmail.com>
2026-02-16 10:20:59 +0700

    p03.1

--- src/practicals/p03/playstation.ts
+++ src/practicals/p03/playstation.ts
@@ -3,9 +3,11 @@ import { Product } from './product'
 export class Playstation extends Product {
   // TODO: implement class properties, constructor with super(...), and methods
   protected name: string = 'Unknown';
+  public generation: number = 0;
 
-  constructor(x: string ,y: number ,z: number) {
+  constructor(name: string ,x: string ,y: number ,z: number) {
+    super(name)
     this.name = x ;
-    this.price =
+    this.generation = 
   }
 }
